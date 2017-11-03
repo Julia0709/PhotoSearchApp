0e7564014fdd24388ae6c8ba121bdd489730c8aa
Julia <berrylily.0709@gmail.com>
2017-11-02 18:12:55 -0700

    Add navigation menu bar

--- PhotoSearchApp/ViewController.swift
+++ PhotoSearchApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Photos"
     }
     
     // Number of sections
